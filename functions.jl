8cc11cac237d3fe35fad2b2a89f49ca8d39e27ca
Jake <jleiser211@gmail.com>
2025-02-16 14:14:50 +0000

    Fixed syntax errors of lift(), flip()

--- functions.jl
+++ functions.jl
@@ -1,6 +1,10 @@
 using Oscar
 
 function witness(h, H, G, ord)
+    # Assume first entry of H is a constant integer h0
+    # Convert H\{h0} to Finite field mod h0
+    # Apply division algorithm to h w.r.t. H\{h0} to get {q1, ..., qn}
+    # Lift back to h_bar = q0*h0 + q1*h1 + ... qn*hn
     u, Q, r = reduce_with_quotients_and_unit(h, H, ordering=ord)
     #println(u)
     @req iszero(r) "Remainder is not zero"
@@ -8,11 +12,12 @@ function witness(h, H, G, ord)
     return sum([qi * gi for (qi, gi) in zip(Q, G)])
 end
 
-function lift(H', ord', H, G, ord)
-    G'' = [witness(h', H, G, ord) for h' in H']
-    #println(G'')
-    G' = reduce(G'', ordering=ord') # Reduce initially?
-    return G'
+
+function lift(Hprime, ordprime, H, G, ord)
+    Gprime = [witness(hprime, H, G, ord) for hprime in Hprime]
+    #println(Gprimeprime)
+    Gprime = reduce(Gprimeprime, ordering=ordprime) # Reduce initially?
+    return Gprime
 end
 
 function flip(G, H, v, ord)
@@ -20,6 +25,6 @@ function flip(G, H, v, ord)
     ord_w = weight_ordering(w, lex(R)) # R?
     ord_wv = weight_ordering(v, ord_w)
 
-    H' = groebner_basis(I, ordering=ord_wv)
-    G' = lift(H', ord_wv, H, G, ord)
+    Hprime = groebner_basis(I, ordering=ord_wv)
+    Gprime = lift(Hprime, ord_wv, H, G, ord)
 end
