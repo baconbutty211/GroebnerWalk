227da1a310b690ee8689b9be3ca7ccec6e1cbd5c
Jake <jleiser211@gmail.com>
2025-02-16 14:15:37 +0000

    New test for witness()

---
+++ test2.jl
@@ -0,0 +1,18 @@
+include("GroebnerWalk.jl")
+
+R, (t, x1, x2, x3, x4) = polynomial_ring(ZZ, ["t", "x1", "x2", "x3", "x4"])
+w = [-1, 1, 11, 3, 19]
+w_x = [0, 1, 1, 1, 1]
+v = -w + 20 * w_x
+#ord = weight_ordering(v, lex(R))
+ord = weight_ordering(w, lex(R))
+
+I = ideal([3 − t, 2 * x1^2 + 3 * x1 * x2 + 24 * x3 * x4, 8 * x1^3 + x2 * x3 * x4 + 18 * x3^2 * x4])
+G = standard_basis(I, ordering=ord)
+H = initial(collect(G), ord, ZZ.(w))
+
+
+print(G[1] == witness(H[1], H, G, ord))
+print(G[2] == witness(H[2], H, G, ord))
+print(G[3] == witness(H[3], H, G, ord))
+print(G[4] == witness(H[4], H, G, ord))
