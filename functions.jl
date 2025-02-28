2818cde7c5ae7f32e7d39c56464cdce651c8d878
Jake <jleiser211@gmail.com>
2025-02-28 22:54:06 +0000

    Replace lift_to_coefficient_ring with Oscar homorphism from finite field to integer ring

--- functions.jl
+++ functions.jl
@@ -1,55 +1,44 @@
 using Oscar
 
-
-function lift_to_coefficient_ring(R, f)
-    C = coefficient_ring(R)
-    if iszero(f)
-        return zero(C)
-    end
-
-    coeffs = [lift(C, c) for c in coefficients(f)]
-    monos = [prod(gens(R) .^ degrees(m)) for m in monomials(f)]
-    fbar = sum(coeffs .* monos)
-
-    #println("coefficients type: ", typeof(coeffs))
-    #println("monomials type: ", typeof(monos))
-    #println("lifted polynomial: ", typeof(fbar))
-
-    return fbar
-end
-
 function witness_optimised(h, H, G, ord)
     # Assume first entry of H is a constant integer h0
     # Convert H\{h0} to Finite field mod h0
-    # R,(x,y) = polynomial_ring(ZZ,["x","y"])
-    # S,(x,y) = polynomial_ring(GF(3),["x","y"])
-    # phi = hom(R,S,c->GF(3)(c),[x,y]) # homomorphism from R to S
-    # Apply division algorithm to h w.r.t. H\{h0} to get {q1, ..., qn}
-    # Lift back to h_bar = q0*h0 + q1*h1 + ... qn*hn
 
+    # Divide h by h0 to get q0
+    # Apply division algorithm to h w.r.t. H\{h0} to get {q1, ..., qn}
+
+    # Lift q1, ..., qn back to original ring (ZZ[x1, ..., xn]) h = q0*h0 + q1*h1 + ... qn*hn
+    # return g = q0*g0 + q1*g1 + ... qn*gn
+
+    #If H contains multiple integers:
     #println(H[1])
     #constants = filter(Oscar.is_constant, H)
     #Hprime = filter(!Oscar.is_constant, H)
 
+    # Assume first entry of H is a constant integer h0
     @req Oscar.is_constant(H[1]) "H[1] is not a constant polynomial"
+    m = coeff(H[1], 1) # 1st coefficient of H[1] is the constant (for type purposes)
+
     R = parent(h)
+    @req typeof(R) == Oscar.ZZMPolyRing "h is not an integer polynomial (in ZZ[x1, ..., xn])"
 
-    c = coeff(H[1], 1) # 1st coefficient of H[1] is the constant (for type purposes)
-    hbar = change_coefficient_ring(GF(c), h)
-    Hbar = [change_coefficient_ring(GF(c), hprime) for hprime in H[2:end]] # Convert to finite field mod c
+    S, _vars = polynomial_ring(GF(m), nvars(R)) # Create polynomial ring over finite field mod m
 
-    q0 = div(h, H[1])
-    u, Q, r = reduce_with_quotients_and_unit(hbar, Hbar, ordering=ord) # Division algorithm (in finite field for performane reasons)
+    phi = hom(R, S, c -> GF(m)(c), gens(S)) # homomorphism from R to S
+    hbar = phi(h) # Convert h to Finite field mod h0
+    Hbar = [phi(_h) for _h in H[2:end]] # Convert H\{h0} to Finite field mod h0
 
-    Q = [lift_to_coefficient_ring(R, q) for q in Q] # Conversion back to original ring (ZZ[x1, ..., xn])
-    Q = pushfirst!(vec(Q), q0)
+    q0 = div(h, H[1]) # Divide h by h0 to get q0
 
-    #println(typeof(r), r)
-    #println(typeof(u), u)
+    u, Q, r = reduce_with_quotients_and_unit(hbar, Hbar, ordering=ord) # Apply division algorithm to h w.r.t. H\{h0} to get {q1, ..., qn} (in finite field for performane reasons)
     @req iszero(r) "Remainder is not zero"
     @req isone(u) "Unit is not one"
 
-    return sum([qi * gi for (qi, gi) in zip(Q, G)]) # sum(Q .* G) - (ERROR) Broadcasting for type Vector{ZZMPolyRingElem} not implemented
+    phi_inv = hom(S, R, c -> lift(ZZ, c), gens(R)) # homomorphism from S to R
+    Q = [phi_inv(q) for q in Q] # Lift q1, ..., qn back to original ring (ZZ[x1, ..., xn])
+    Q = pushfirst!(vec(Q), q0)
+
+    return sum([qi * gi for (qi, gi) in zip(Q, G)]) # g = q0*g0 + q1*g1 + ... qn*gn. sum(Q .* G) - (ERROR) Broadcasting for type Vector{ZZMPolyRingElem} not implemented
 end
 
 function witness(h, H, G, ord)
