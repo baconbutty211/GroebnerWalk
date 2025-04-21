658319550196312eb7ca16d42f0ef16343a20943
Jake <jleiser211@gmail.com>
2025-04-21 20:26:24 +0100

    Implemented a bunch of helper functions including isReduced & isInitiallyReduced

--- functions.jl
+++ functions.jl
@@ -1,3 +1,5 @@
+using Oscar
+
 function lt(f::MPolyRingElem, ord::MonomialOrdering)
     # Get the leading term of a polynomial f w.r.t. an ordering ord
     # Input: f ∈ R[t, x1, ..., xn], ord a t-local monomial ordering on R
@@ -16,7 +18,6 @@ function lt(f::MPolyRingElem, ord::MonomialOrdering)
     end
     return lead_term
 end
-
 function lm(f::MPolyRingElem, ord::MonomialOrdering)
     monomial(lt(f, ord), 1)
 end
@@ -24,13 +25,38 @@ function lc(f::MPolyRingElem, ord::MonomialOrdering)
     coeff(lt(f, ord), 1) # Get the coefficient of the leading term of f w.r.t. ord
 end
 
-function getXExponents(term::MPolyRingElem)
+function getExponents(term::MPolyRingElem)
     # Get the exponents of x1, ..., xn from a term in R[t, x1, ..., xn]
     # Input: term ∈ R[t, x1, ..., xn]
     # Output: exponents ∈ ZZ^n is the exponent vector of x1, ..., xn
     @req length(exponents(term)) > 0 "Length of exponents is not greater than 0"
-    return collect(exponents(t))[1][2:end] # Return the exponent vector excluding t
+    return collect(exponents(term))[1] # Return the exponent vector excluding t
 end
+function getXExponents(term::MPolyRingElem)
+    # Get the exponents of x1, ..., xn from a term in R[t, x1, ..., xn]
+    # Input: term ∈ R[t, x1, ..., xn]
+    # Output: exponents ∈ ZZ^n is the exponent vector of x1, ..., xn
+    return getExponents(term)[2:end] # Return the exponent vector excluding t
+end
+function getTExponent(term::MPolyRingElem)
+    # Get the exponents of x1, ..., xn from a term in R[t, x1, ..., xn]
+    # Input: term ∈ R[t, x1, ..., xn]
+    # Output: exponents ∈ ZZ^n is the exponent vector of x1, ..., xn
+    return getExponents(term)[1] # Return the exponent vector excluding t
+end
+
+function getTCoefficient(term::MPolyRingElem)
+    # Get the coefficient from a term in R[t, x1, ..., xn]
+    # Input: term ∈ R[t, x1, ..., xn]
+    # Output: coeff ∈ R[t] is the coefficient of term
+    @req length(exponents(term)) > 0 "Length of exponents is not greater than 0"
+    c = coeff(term, 1) # Get the coefficient of the term
+    @req c !== 0 "Coefficient is zero"
+
+    alpha_prime = collect(exponents(t))[1][1] # Return the coefficient of t
+    return c * t^alpha_prime # Return the coefficient in R[t]
+end
+
 function collectXTerms(f::MPolyRingElem, alpha::Vector{Int})
     # Assume t variable is the first variable in the polynomial ring
     # Collect the terms of f with same  x1, ..., xn exponent vector alpha
@@ -54,4 +80,105 @@ function collectLeadingXTerms(f::MPolyRingElem, ord::MonomialOrdering)
     @req length(f) > 0 "Number of terms is not greater than 0"
     alpha = getXExponents(lt(f, ord)) # Get the exponent vector of the leading term of f w.r.t. lex ordering
     return collectXTerms(f, alpha) # Return the sum of the terms with same exponent vector alpha
+end
+
+function collectLeadingXCoefficients(f::MPolyRingElem, ord::MonomialOrdering)
+    # Assume t variable is the first variable in the polynomial ring
+    # Collect the leading coefficient of f w.r.t. ord
+    # Input: f ∈ R[t, x1, ..., xn]
+    # Output: lc_alpha ∈ R[t] is the leading coefficient of f w.r.t. ord
+    @req length(f) > 0 "Number of terms is not greater than 0"
+    g = collectLeadingXTerms(f, ord) # Collect the leading x-terms of f w.r.t. ord
+    alpha = getXExponents(lt(g, ord)) # Get the exponent vector of the leading term of g w.r.t. lex ordering
+    x_vars = gens(parent(g))[2:end] # Get the variables of the polynomial ring
+    x_alpha = prod(x_vars .^ alpha)
+    Q, r = reduce_with_quotients(g, x_alpha, ordering=ord) # Apply division algorithm to g w.r.t. x_vars to get {q1, ..., qn} (in finite field for performance reasons)
+
+    @req iszero(r) "Remainder is not zero"
+    @req length(Q) == 1 "Length of Q is not equal to 1"
+
+    return Q[1]
+end
+
+function collectCoefficients(f::MPolyRingElem, ord::MonomialOrdering)
+    # Assume t variable is the first variable in the polynomial ring
+    # Input: f ∈ R[t, x1, ..., xn]
+    # Output: g ∈ R[t][x1, ..., xn]
+    @req length(f) > 0 "Number of terms is not greater than 0"
+    f_prime = [] # Initialize f' to lt(f, ord)
+    f_primeprime = f # Initialize f'' to f - lt(f, ord)
+
+    x_vars = gens(parent(f))[2:end] # Get the variables of the polynomial ring
+    while !iszero(f_primeprime)
+        f_lt = lt(f_primeprime, ord) # Get the leading term of f w.r.t. ord
+        alpha = getXExponents(f_lt) # Get the exponent vector of the leading term of term w.r.t. lex ordering
+        x_alpha = prod(x_vars .^ alpha) # Calculate x^alpha
+        Q, r = reduce_with_quotients(f_primeprime, [x_alpha], ordering=ord) # Apply division algorithm to g w.r.t. x_vars to get {q1, ..., qn} (in finite field for performance reasons)
+
+        @req length(Q) == 1 "Length of Q is not equal to 1"
+
+        pushfirst!(f_prime, (Q[1], x_alpha)) # Add (f_alpha, x^alpha) to f'
+        f_primeprime = r # Subtract the remainder from f''
+    end
+    return f_prime # Return f' which is the list of the terms f_alpha(t) * x^alpha in f in the form [(f_alpha, x^alpha), ...] 
+end
+
+function sortByLeadingMonomial(F, ord::MonomialOrdering)
+    # Sort the elements of F by their leading monomial w.r.t. ord
+    # Input: F ∈ R[t, x1, ..., xn]
+    # Output: F_sorted ∈ R[t, x1, ..., xn] is the sorted list of elements of F by their leading monomial w.r.t. ord
+    @req length(F) > 0 "Number of terms is not greater than 0"
+    @req length(F) == length(unique(F)) "F contains duplicate elements"
+
+    F_sorted = []
+    for f in F
+        if length(F_sorted) == 0
+            push!(F_sorted, f) # Add the first element to the sorted list
+        else
+            i = 1 # Initialize index to 1
+            while i <= length(F_sorted) && cmp(ord, lm(F_sorted[i], ord), lm(f, ord)) == 1
+                i += 1 # Increment index until the correct position is found
+            end
+            insert!(F_sorted, i, f) # Insert f at the correct position in the sorted list
+        end
+    end
+    return F_sorted # Return the sorted list of elements of F by their leading monomial w.r.t. ord
+end
+
+function isReduced(G, ord)
+    # Check if G is reduced w.r.t. ord
+    # Input: G ∈ R[t, x1, ..., xn], ord a t-local monomial ordering on R
+    # Output: true if G is reduced w.r.t. ord, false otherwise
+    @req length(G) > 0 "Number of terms is not greater than 0"
+    G_lm = [lm(_g, ord) for _g in G] # Get the leading terms of G w.r.t.
+    for g in G
+        g_lm = lm(g, ord) # Get the leading term of g w.r.t. ord
+        G_minus_g_lm = filter(_lm -> _lm != g_lm, G_lm) # Get the leading terms of G without g
+        Q, r = reduce_with_quotients(g, G_minus_g_lm, ordering=ord)
+        #println("g = ", g, ", G_minus_g_lm = ", G_minus_g_lm, ", Q = ", Q, ", r = ", r)
+        if r != g # Check if g divides a term in g'
+            return false
+        end
+    end
+    return true # G is reduced w.r.t. ord
+end
+function isInitiallyReduced(G, ord)
+    # Check if G is initially reduced w.r.t. p-t, H, G under >
+    # Input: G ∈ R[t, x1, ..., xn], H ∈ R[t, x1, ..., xn], p a prime number, > a t-local monomial ordering on R
+    # Output: true if G is initially reduced w.r.t. p-t under >, false otherwise
+    @req length(G) > 0 "Number of terms is not greater than 0"
+    @req length(H) > 0 "Number of terms is not greater than 0"
+
+    G_prime = []
+    for g in G
+        g_collected = collectCoefficients(g, ord) # Collect the coefficients of g w.r.t. ord
+        g_prime = 0
+        for (g_alpha, x_alpha) in g_collected
+            g_alpha_lt = lt(g_alpha, ord) # Get the leading term of g_alpha w.r.t. ord
+            g_prime += g_alpha_lt * x_alpha # Add the coefficients of g w.r.t. ord
+        end
+        push!(G_prime, g_prime) # Add the leading term of g to G'
+    end
+
+    return isReduced(G_prime, ord) # G is initially reduced w.r.t. p-t under >
 end
